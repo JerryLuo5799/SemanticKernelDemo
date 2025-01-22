ba07a860e93873144e276defbab1f81f88b90290
Jerry Luo <jerryluo@ssw.com.au>
2025-01-22 15:03:08 +0800

    添加导入OpenAPI(Swagger)文档功能

---
+++ Module.API/Controllers/ScheduleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Module.API.Controllers
+{
+    /// <summary>
+    /// 用户日程相关接口
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    public class ScheduleController : ControllerBase
+    {
+        /// <summary>
+        /// 获取用户今天的日程数量
+        /// </summary>
+        /// <returns>当前用户今天的日程数量</returns>
+        [HttpGet]
+        [Route("GetScheduleCount")]
+        public string GetScheduleCount()
+        {
+            Random random = new Random();
+            return $"你 {DateTime.Now.ToString("yyyy-MM-dd")} 有 {random.Next(10)} 个日程";
+        }
+    }
+}
--- SemanticKernelDemo/Program.cs
+++ SemanticKernelDemo/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using SemanticKernelDemo.Plugin;
 using Microsoft.SemanticKernel.Plugins.Core;
+using Microsoft.SemanticKernel.Plugins.OpenApi; // Add this using directive
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.InputEncoding = System.Text.Encoding.UTF8;
@@ -17,6 +18,9 @@ builder.Plugins.AddFromType<TimePlugin>("Time");
 
 // Build the kernel
 Kernel kernel = builder.Build();
+
+await kernel.ImportPluginFromOpenApiAsync("lights", new Uri("https://localhost:7293/swagger/v1/swagger.json"), new OpenApiFunctionExecutionParameters() { EnablePayloadNamespacing = true });
+
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
 foreach (var plugin in kernel.Plugins)
@@ -36,10 +40,10 @@ PromptExecutionSettings executionSettings = new()
 
 // Create a history store the conversation
 var history = new ChatHistory($"""
-    你是一个AI助手。你总是会调用插件来回答用户的问题, 如果用户的请求可以通过插件来满足, 那就只调用插件。你不会更改插件的输出格式。
-    你不会输出markdown格式。
-    如果遇到"今天", "昨天"这样的日期描述，请根据今天的实际日期, 将其转换为具体日期
-    """);
+        你是一个AI助手。你总是会调用插件来回答用户的问题, 如果用户的请求可以通过插件来满足, 那就只调用插件。你不会更改插件的输出格式。
+        你不会输出markdown格式。
+        如果遇到"今天", "昨天"这样的日期描述，请根据今天的实际日期, 将其转换为具体日期
+        """);
 
 
 // Initiate a back-and-forth chat
@@ -52,8 +56,8 @@ do
 
     userInput = Console.ReadLine();
     // Add user input
-    history.AddUserMessage(userInput);
-    
+    history.AddUserMessage(userInput ?? string.Empty); // Fix for possible null reference
+
     // Get the response from the AI
     var result = await chatCompletionService.GetChatMessageContentAsync(history, executionSettings, kernel);
 
@@ -66,4 +70,3 @@ do
     // Add the message from the agent to the chat history
     history.AddMessage(result.Role, result.Content ?? string.Empty);
 } while (userInput is not null);
-
